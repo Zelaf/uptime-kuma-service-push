c38d10020768bd876ca1c9c3abcac127d9336c80
Zelaf <zelaf@zelaf.eu>
2023-07-31 19:30:37 +0200

    Changed the sudo example

--- src/initialize.sh
+++ src/initialize.sh
@@ -1,6 +1,10 @@
 if [ "$EUID" -ne 0 ]
-  then echo "This script needs to be run as root to be able to read the available services."
-  echo "Example:\n"
-  echo "sudo ./uptime-kuma-service-push"
+  then 
+  echo
+  echo "This script needs to be run as root to be able to read the available services."
+  echo
+  echo "Example:"
+  echo "sudo ${PWD}/uptime-kuma-service-push"
+  echo
   exit
 fi
