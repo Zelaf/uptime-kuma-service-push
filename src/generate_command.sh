bff70e8dc8427615ef4be67883622416e41a6dc1
Zelaf <zelaf@zelaf.eu>
2023-07-31 23:46:54 +0200

    Changed commenting & echos, removed `inspect_args`

--- src/generate_command.sh
+++ src/generate_command.sh
@@ -1,20 +1,21 @@
-echo "# this file is located in 'src/generate_command.sh'"
-echo "# code for 'uptime-kuma-service-push generate' goes here"
-echo "# you can edit it freely and regenerate (it will not be overwritten)"
-inspect_args
+# echo "# this file is located in 'src/generate_command.sh'"
+# echo "# code for 'uptime-kuma-service-push generate' goes here"
+# echo "# you can edit it freely and regenerate (it will not be overwritten)"
 
-#Variables - output file name
+# Variables - output file name
 script_file=uptime-kuma-service-push_${args[service]}
 
-#Calls to script_output that generates the script file
+echo
+echo
+
+# Calls to script_output that generates the script file
 write_script
 
-echo
 echo
 echo "Finished!"
 echo
 echo
-echo "Your monitor script has been created in '$(readlink -f uptime-kuma-service-push_docker)'"
+echo "Your monitor script has been created in '$(readlink -f uptime-kuma-service-push_${args[service]})'"
 echo
 echo "To use it simply run '${script_file}' as root."
 echo
--- src/initialize.sh
+++ src/initialize.sh
@@ -3,7 +3,7 @@ if [ "$EUID" -ne 0 ]
   echo
   echo "This script needs to be run as root to be able to read the available services."
   echo
-  echo "Example:"
+  echo "To quickly run as root, run this command:"
   echo "sudo ${PWD}/uptime-kuma-service-push"
   echo
   exit
