674758ab96240fe8d58100b4aeae4a3ad2c6c479
Zelaf <zelaf@zelaf.eu>
2023-08-01 22:12:38 +0200

    Added URL validation, cleaned up help.

---
+++ src/lib/validations/generate/validate_url.sh
@@ -0,0 +1,9 @@
+## Checks if the URL is valid but doing a curl before generating the script
+## src/lib/validations/url/validate_url.sh
+## This function is responsible for URL validation
+validate_url() {
+
+  ## This condition checks if the URL is in a valid format, writes error message if not.
+  [[ "${1}" =~ ^(http|https)://.*/api/push/.*$ ]] || echo -e "\n'${1}' is not a valid URL \nSee 'uptime-kuma-service-push generate --help'"
+
+}
