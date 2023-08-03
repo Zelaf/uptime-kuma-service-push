2c79823019eb1d676472c2279be8407037d3cdcd
Zelaf <zelaf@zelaf.eu>
2023-08-03 22:04:47 +0200

    Deleted unnecessary files

--- src/commands/config.sh
+++
@@ -1,6 +0,0 @@
-echo "# this file is located in 'src/commands/config.sh'"
-echo "# code for 'uptime-kuma-service-push config' goes here"
-echo "# you can edit it freely and regenerate (it will not be overwritten)"
-inspect_args
-
-yaml_load "config.yml"
