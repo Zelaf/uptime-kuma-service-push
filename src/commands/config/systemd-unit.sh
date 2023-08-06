1bbc745d1a9ed1a5f08926bea5c0fa6aecaca802
Zelaf <zelaf@zelaf.eu>
2023-08-07 01:30:21 +0200

    Added systemd unit configurator

--- src/commands/config/systemd-unit.sh
+++ src/commands/config/systemd-unit.sh
@@ -1,5 +1,15 @@
-echo "# this file is located in 'src/commands/config/systemd-unit.sh'"
-echo "# code for 'uptime-kuma-service-push config systemd-unit' goes here"
-echo "# you can edit it freely and regenerate (it will not be overwritten)"
-inspect_args
-config_show
+## this file is located in 'src/commands/config/systemd-unit.sh'"
+## code for 'uptime-kuma-service-push config systemd-unit' goes here"
+## you can edit it freely and regenerate (it will not be overwritten)"
+
+echo "Installing systemd unit files..."
+
+write_systemd_unit
+
+echo "Enabling systemd unit..."
+systemctl daemon-reload
+systemctl enable uptime-kuma-service-push.timer --now
+systemctl enable uptime-kuma-service-push.service --now
+
+echo "Finished!"
+echo "Your services will now be monitored by systemd."
