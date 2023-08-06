8d82b7fa813895f225930de0b2712dc816852a07
Zelaf <zelaf@zelaf.eu>
2023-08-07 01:31:33 +0200

    Removed old command names and filter names

--- src/commands/monitor/add.sh
+++
@@ -1,4 +0,0 @@
-echo "# this file is located in 'src/commands/monitor/add.sh'"
-echo "# code for 'uptime-kuma-service-push monitor add' goes here"
-echo "# you can edit it freely and regenerate (it will not be overwritten)"
-inspect_args
--- src/lib/filters/filters_folder_exists.sh
+++
@@ -1,6 +0,0 @@
-## Checks if the folder is valid but doing a curl before generating the script
-## src/lib/validations/generate/validate_folder.sh
-## This function is responsible for folder validation
-filter_folder_exists() {
-    [[ -d "$SCRIPT_PATH/$(config_get "generate.folder_location")" ]] || mkdir "$SCRIPT_PATH/$(config_get "generate.folder_location")"
-}
