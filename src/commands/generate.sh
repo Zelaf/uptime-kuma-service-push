ac57b696bb81b526e3fa8f328a6ab0310758e37b
Zelaf <zelaf@zelaf.eu>
2023-08-07 01:28:59 +0200

    Changed variable names

--- src/commands/generate.sh
+++ src/commands/generate.sh
@@ -9,15 +9,15 @@ local url
 url=${args[url]}
 local script_file
 script_file=$(config_get "generate.script_name")_"$service".sh
-local script_folder
-script_folder=$SCRIPT_PATH/$(config_get "generate.directory_name")
+local script_directory
+script_directory=$SCRIPT_PATH/$(config_get "generate.directory_name")
 
 ## Calls the write_script function that generates the script file
 write_script
 
 ## Adds execution permission so it can run
 printf "Adding execution permissions..."
-chmod +x "${script_folder}/${script_file}"
+chmod 700 "${script_directory}/${script_file}"
 
 ## Tells the user that the script is finished
-printf '%s' "\nFinished!\n\nYour monitor script has been created in '$(readlink -f "${script_folder}/${script_file}")'\nTo use it simply run it as root.\n\nYou can add it to systemd, cron, or your preferred method to automate the execution.\n"
+printf "\nFinished!\n\nYour monitor script has been created in '$(readlink -f "${script_directory}/${script_file}")'\nTo use it simply run it as root.\n\nYou can add it to systemd, cron, or your preferred method to automate the execution.\n"
