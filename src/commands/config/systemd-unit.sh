7f6298b95b982109c0259d81bad87420b3bbb0c7
Zelaf <zelaf@zelaf.eu>
2023-08-06 15:27:27 +0200

    Empty systemd-unit command

---
+++ src/commands/config/systemd-unit.sh
@@ -0,0 +1,4 @@
+echo "# this file is located in 'src/commands/config/systemd-unit.sh'"
+echo "# code for 'uptime-kuma-service-push config systemd-unit' goes here"
+echo "# you can edit it freely and regenerate (it will not be overwritten)"
+inspect_args
