957ec2d5ea80804ed561b622cdeb016a23d75297
Zelaf <zelaf@zelaf.eu>
2023-08-06 18:16:41 +0200

    Monitoring commands

---
+++ src/commands/monitor/add.sh
@@ -0,0 +1,4 @@
+echo "# this file is located in 'src/commands/monitor/add.sh'"
+echo "# code for 'uptime-kuma-service-push monitor add' goes here"
+echo "# you can edit it freely and regenerate (it will not be overwritten)"
+inspect_args
