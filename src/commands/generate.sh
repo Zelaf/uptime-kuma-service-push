033fe5656dc5d2f951ec835a9496e0876081e4a3
Zelaf <zelaf@zelaf.eu>
2023-08-06 18:15:35 +0200

    Cleaned up generate script syntaxing

--- src/commands/generate.sh
+++ src/commands/generate.sh
@@ -1,22 +1,23 @@
-## this file is located in 'src/generate_command.sh'"
-## code for 'uptime-kuma-service-push generate' goes here"
-## you can edit it freely and regenerate (it will not be overwritten)"
+## This file is located in 'src/generate_command.sh'
+## Code for 'uptime-kuma-service-push generate' goes here
+## You can edit it freely and regenerate (it will not be overwritten)
 
 ## Variables
-local service=${args[service]}
-local url=${args[url]}
-local script_file=$(config_get "generate.file_name")_$service.sh
-local script_folder=$SCRIPT_PATH/$(config_get "generate.folder_location")
+local service
+service=${args[service]}
+local url
+url=${args[url]}
+local script_file
+script_file=$(config_get "generate.script_name")_"$service".sh
+local script_folder
+script_folder=$SCRIPT_PATH/$(config_get "generate.directory_name")
 
-## Calls to the write_script function that generates the script file
+## Calls the write_script function that generates the script file
 write_script
-echo
 
-## Execution permission so it can run
+## Adds execution permission so it can run
 printf "Adding execution permissions..."
-chmod +x ${script_folder}/${script_file}
+chmod +x "${script_folder}/${script_file}"
 
-## Tell user that the script is finnished
-## Shorter version
-printf "\nFinished!\n\nYour monitor script has been created in '$(readlink -f ${script_folder}/${script_file})'\nTo use it simply run it as root.\n\nYou can add it to systemd, cron or your preferred method to automate the execution.\n"
-echo
+## Tells the user that the script is finished
+printf '%s' "\nFinished!\n\nYour monitor script has been created in '$(readlink -f "${script_folder}/${script_file}")'\nTo use it simply run it as root.\n\nYou can add it to systemd, cron, or your preferred method to automate the execution.\n"
