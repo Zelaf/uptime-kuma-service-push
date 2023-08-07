4e007fb10f21deeef2425060b4a815482650503f
Zelaf <zelaf@zelaf.eu>
2023-08-07 18:46:40 +0200

    Removed old settings, added new settings. Interval and logs can be chosen now.

--- src/lib/functions/generate/write_script.sh
+++ src/lib/functions/generate/write_script.sh
@@ -10,6 +10,8 @@
 ##
 write_script() {
 
+## Append logs if generate.append_logs is true
+if [[ $(config_get generate.append_logs) == "true" ]]; then
   ## Generate script
   write_script=$(
     cat <<-EOF
@@ -26,9 +28,30 @@ write_script() {
  fi
 EOF
   )
+fi
+
+## Don't append if generate.append_logs is false
+if [[ $(config_get generate.append_logs) == "false" ]]; then
+  ## Generate script
+  write_script=$(
+    cat <<-EOF
+ #!/usr/bin/env bash
+ 
+ # Made using
+ # Uptime-Kuma-Service-Push
+ # https://github.com/Zelaf/uptime-kuma-service-push
+
+ if systemctl is-active --quiet ${service}; then
+   curl -G ${url%%'?'*}?status=up --silent --output /dev/null --data-urlencode "msg=${service} - running"
+ else
+   curl -G ${url%%'?'*}?status=down --silent --output /dev/null --data-urlencode "msg=${service} - not running."
+ fi
+EOF
+  )
+fi
 
   ## Write the script to the new file
-  printf "Generating push-script...\n"
+  printf "Generating push script...\n"
   echo "${write_script}" >"${script_directory}/${script_file}"
 
 }
