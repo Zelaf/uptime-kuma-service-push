25389bd89a6c25407531b67b55033ff810a2b6c4
Zelaf <zelaf@zelaf.eu>
2023-10-02 13:50:14 +0200

    Renamed filter for clearer name.

--- src/lib/filters/filters_generate_directory_exists.sh
+++ src/lib/filters/filters_generate_directory_exists.sh
@@ -1,6 +1,6 @@
 ## Checks if the directory is valid but doing a curl before generating the script
 ## src/lib/filters/filters_directory_exists.sh
 ## This function is responsible for directory validation
-filter_directory_exists() {
+filter_generate_directory_exists() {
     [[ -d "$SCRIPT_PATH/${GENERATE_DIRECTORY}" ]] || mkdir "$SCRIPT_PATH/${GENERATE_DIRECTORY}"
 }
