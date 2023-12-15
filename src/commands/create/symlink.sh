343c4efb5e0d7aede2fbc7ffd8f6d153f4e0ee79
Zelaf <zelaf@zelaf.eu>
2023-12-15 17:58:39 +0100

    Created symlink command.

---
+++ src/commands/create/symlink.sh
@@ -0,0 +1,9 @@
+## code for 'uptime-kuma-service-push create symlink' goes here"
+## you can edit it freely and regenerate (it will not be overwritten)"
+## this file is located in 'src/commands/create/symlink.sh'"
+inspect_args
+
+echo "Creating symlink for Uptime-Kuma-Service-Push..."
+ln -s -f "$new_folder"/uptime-kuma-service-push /usr/local/bin/uptime-kuma-service-push
+echo "Finished! You can now run Uptime-Kuma-Service-Push using 'uptime-kuma-service-push' directly in the terminal."
+echo
