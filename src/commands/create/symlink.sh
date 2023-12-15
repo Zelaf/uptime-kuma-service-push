5aa81ce5d348dc22229983422de5784c6e391f32
Zelaf <zelaf@zelaf.eu>
2023-12-15 18:05:43 +0100

    Added remove flag

--- src/commands/create/symlink.sh
+++ src/commands/create/symlink.sh
@@ -3,6 +3,15 @@
 ## this file is located in 'src/commands/create/symlink.sh'"
 inspect_args
 
+if [[ -f "/usr/local/bin/uptime-kuma-service-push" && ${args[--remove]} ]]; then
+    rm /usr/local/bin/uptime-kuma-service-push && printf "Removing symlink...\n"
+    echo "Finished!"
+    exit 0
+else
+    echo "No existing link found..."
+    echo "Finished!"
+fi
+
 echo "Creating symlink for Uptime-Kuma-Service-Push..."
 ln -s -f "$SCRIPT_PATH"/uptime-kuma-service-push /usr/local/bin/uptime-kuma-service-push
 echo "Finished! You can now run Uptime-Kuma-Service-Push using 'uptime-kuma-service-push' directly in the terminal."
