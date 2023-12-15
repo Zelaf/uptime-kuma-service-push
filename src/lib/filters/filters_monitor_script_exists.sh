31df89952aba1d91f8cddb29d82f28c9c63a94d6
Zelaf <zelaf@zelaf.eu>
2023-12-15 19:57:42 +0100

    Fixed filepath and changed error output to new command.

--- src/lib/filters/filters_monitor_script_exists.sh
+++ src/lib/filters/filters_monitor_script_exists.sh
@@ -2,5 +2,5 @@
 ## src/lib/filters/filters_directory_exists.sh
 ## This function is responsible for directory validation
 filter_monitor_script_exists() {
-    [[ -f "$SCRIPT_PATH/${MONITOR_NAME}" ]] || printf "Monitor script does not exist, please see 'uptime-kuma-service-push create monitor --help'\n"
+    [[ -f "$SCRIPT_PATH/${MONITOR_NAME}.sh" ]] || printf "Monitor script does not exist, please see 'uptime-kuma-service-push create monitor --help'\n"
 }
